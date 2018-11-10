f37dd169737aba28fea8a48e8385bca9eb7468fe
pierreboissinot <perso@pierreboissinot.me>
2018-11-10 18:21:37 +0100

    add GetFolder example

---
+++ examples/folders.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"go-wrike"
+	"log"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	wrikec := wrike.NewClient(nil, os.Getenv("WRIKE_API_TOKEN"))
+	folder, _, _ := wrikec.Folders.GetFolder(os.Getenv("WRIKE_FOLDER"))
+	fmt.Print(folder.Data[0].Title)
+}
