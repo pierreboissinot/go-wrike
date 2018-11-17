5c157c41fd2aa3e0a11b5b2740839bc252ad8646
Pierre Boissinot <perso@pierreboissinot.me>
2018-11-17 14:55:09 +0100

    Revert "fix string"
    This reverts commit 88b40072

--- timelog.go
+++ timelog.go
@@ -26,7 +26,7 @@ type Timelogs struct {
 }
 
 func (s *TimelogService) GetTimelogs(id string) (*Timelogs, *Response, error) {
-	u := fmt.Sprintf("folders/%s", id) + "/timelogs"
+	u := fmt.Sprintf("folders/%s/timelogs", id)
 	fmt.Println(u)
 	req, err := s.client.NewRequest("GET", u)
 	if err != nil {
